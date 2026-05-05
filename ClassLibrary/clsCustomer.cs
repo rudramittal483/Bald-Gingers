630334c56bbd58b5fa49544dd2932ac7637942fa
Taifu378 <taifu378@gmail.com>
2026-05-05 15:39:59 +0400

    Customer properties

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public int CustomerNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateJoined { get; set; }
+        public bool IsActiveAccount { get; set; }
     }
 }
