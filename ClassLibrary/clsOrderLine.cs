f9ab853fd4cba369fb3b612fac774b05ab7a1e31
p2897241_dmu <p2897241_dmu@apporto.com>
2026-05-05 16:31:56 +0530

    Added properties to order and orderLine

---
+++ ClassLibrary/clsOrderLine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrderLine
+    {
+        public int OrderLineNo { get; set; }
+        public int OrderNo { get; set; }
+        public int LaptopNo { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Testing4/tstOrderLine.cs
@@ -0,0 +1,53 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestingOrders
+{
+    [TestClass]
+    public class tstOrderLine
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Assert.IsNotNull(AnOrderLine);
+        }
+
+        [TestMethod]
+        public void OrderLineNoPropertyOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Int32 TestData = 1;
+            AnOrderLine.OrderLineNo = TestData;
+            Assert.AreEqual(AnOrderLine.OrderLineNo, TestData);
+        }
+
+        [TestMethod]
+        public void OrderNoPropertyOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Int32 TestData = 1;
+            AnOrderLine.OrderNo = TestData;
+            Assert.AreEqual(AnOrderLine.OrderNo, TestData);
+        }
+
+        [TestMethod]
+        public void LaptopNoPropertyOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Int32 TestData = 1;
+            AnOrderLine.LaptopNo = TestData;
+            Assert.AreEqual(AnOrderLine.LaptopNo, TestData);
+        }
+
+        [TestMethod]
+        public void QuantityPropertyOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Int32 TestData = 1;
+            AnOrderLine.Quantity = TestData;
+            Assert.AreEqual(AnOrderLine.Quantity, TestData);
+        }
+    }
+}
