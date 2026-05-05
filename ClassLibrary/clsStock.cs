2eccf8c9b3e847b153cac16c027c962284ec1d68
rudramittal483 <rudramittal11@gmail.com>
2026-05-05 16:24:18 +0530

    Added properties to stock

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool InStock { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int LaptopId { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public double Price { get; set; }
     }
 }
