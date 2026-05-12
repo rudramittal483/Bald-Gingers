71e8d9f1c1eeeb8330e8733829753c9e9bdb2444
rudramittal483 <rudramittal11@gmail.com>
2026-05-12 18:28:06 +0530

    added date validaiton (others remaining)

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -131,5 +131,40 @@ namespace ClassLibrary
 
 
         }
+
+        public string Valid(string model, string brand, string price, string dateAdded)
+        {
+            //create a string variable to store the error
+            String Error = "";
+            //create a temporary variable to store date values
+            DateTime DateTemp;
+
+            //create an instance of DateTime to compare with DateTemp
+            //in the if statement
+            DateTime DateComp = DateTime.Now.Date;
+            try
+            {
+                //copy the dateAdded value to the DateTemp variable
+                DateTemp = Convert.ToDateTime(dateAdded);
+                if (DateTemp < DateComp) //compare dateAdded with Date
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the past : ";
+                }
+                //check to see if the date is greater than today's date
+                if (DateTemp > DateComp)
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the future : ";
+                }
+            }
+            catch
+            {
+                //record the error
+                Error = Error + "The date was not a valid date : ";
+            }
+            //return any error messages
+            return Error;
+        }
     }
 }
