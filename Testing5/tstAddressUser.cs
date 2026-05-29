0e7ed1eb71f548f5c8a71c8ac9761ccbae9ee41f
rudramittal483 <rudramittal11@gmail.com>
2026-05-29 15:55:27 +0530

    added customer and address login pages

---
+++ Testing5/tstAddressUser.cs
@@ -0,0 +1,110 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstAddressUser
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsAddressUser AnUser = new clsAddressUser();
+            //test to see that the two values are the same
+            Assert.IsNotNull(AnUser);
+        }
+
+        [TestMethod]
+        public void UserIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsAddressUser AnUser = new clsAddressUser();
+            //create some test data to assign to the property
+            Int32 TestData = 3;
+            //assign the data to the property
+            AnUser.UserID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.UserID, TestData);
+        }
+
+        [TestMethod]
+        public void UserNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsAddressUser AnUser = new clsAddressUser();
+            //create some test data to assign to the property
+            string TestData = "Atif";
+            //assign the data to the property
+            AnUser.UserName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.UserName, TestData);
+        }
+
+
+        [TestMethod]
+        public void PasswordPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsAddressUser AnUser = new clsAddressUser();
+            //create some test data to assign to the property
+            string TestData = "fail123";
+            //assign the data to the property
+            AnUser.Password = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.Password, TestData);
+        }
+
+
+        [TestMethod]
+        public void DepartmentPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsAddressUser AnUser = new clsAddressUser();
+            //create some test data to assign to the property
+            string TestData = "Customer";
+            //assign the data to the property
+            AnUser.Department = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.Department, TestData);
+        }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsAddressUser AnUser = new clsAddressUser();
+            //boolean variable to store the results of the validation
+            Boolean Found = false;
+            //create some test data to use with the method
+            string UserName = "Atif";
+            string Password = "fail123";
+            //invoke the method
+            Found = AnUser.FindUser(UserName, Password);
+            //test to see that the result is true
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestUserNamePWFound()
+        {
+            //create an instance of the class we want to create
+            clsAddressUser AnUser = new clsAddressUser();
+            //boolean variable to store the results of the validation
+            Boolean Found = false;
+            Boolean OK = true;
+            //create some test data to use with the method
+            string UserName = "Atif";
+            string Password = "fail123";
+            //invoke the method
+            Found = AnUser.FindUser(UserName, Password);
+            //check the user id property
+            if (AnUser.UserName != UserName && AnUser.Password != Password)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+    }
+}
